bda2bb24a7c96ddb43530267b520b0d5b92c535a
mike <viriyananda.michael@gmail.com>
2017-01-10 13:00:07 +0700

    Adding new module account_budget_variance
    Revision
    
    Avoiding division by zero
    
    Avoiding division by zero

---
+++ account_budget_variance/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Budget Variance",
+    "version": "8.0.1.0.0",
+    "summary": "Adds variance amount and percent",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "account_budget",
+    ],
+    "data": [
+        "views/account_budget_post_view.xml",
+        "views/crossovered_budget_view.xml",
+        "views/crossovered_budget_line_view.xml"
+    ],
+}
---
+++ account_budget_variance/tests/test_compute.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Michael Viriyananda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestCompute(TransactionCase):
+    def setUp(self, *args, **kwargs):
+        super(TestCompute, self).setUp(*args, **kwargs)
+        # Objects
+        self.obj_budget = self.env['crossovered.budget.lines']
+
+    def test_compute(self):
+        new = self.obj_budget.new()
+        new.planned_amount = 50000
+
+        # Check Variance Amount
+        self.assertEqual(-50000, new.variance_amount)
+
+        # Check Variance Percent
+        self.assertEqual(-1, new.variance_percent)
