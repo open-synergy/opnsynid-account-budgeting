a2790d39fcf04ff571edff0d6f26588c862af9c1
mike <viriyananda.michael@gmail.com>
2017-07-20 17:20:07 +0700

    [MIG] Migration to v9

--- account_budget_variance/__openerp__.py
+++ account_budget_variance/__openerp__.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Budget Variance",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "summary": "Adds variance amount and percent",
     "category": "Accounting",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "application": False,
     "depends": [
         "account_budget",
     ],
     "data": [
-        "views/account_budget_post_view.xml",
         "views/crossovered_budget_view.xml",
         "views/crossovered_budget_line_view.xml"
     ],
