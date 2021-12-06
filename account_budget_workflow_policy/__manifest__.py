f02ef93f37d2e7a9621ba7f58c547e0cf5a35d4d
Asrulbastian <asrulbastianyunas@gmail.com>
2021-12-06 20:52:58 +0700

    [account_budget_workflow_policy] 11.0.1.0.0

---
+++ account_budget_workflow_policy/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Account Budget Workflow Policy",
+    "version": "11.0.1.0.0",
+    "category": "Administrator",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "account_budget",
+        "account_budget_multiple_approval",
+        "ssi_policy_mixin",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_budget_views.xml",
+    ],
+}
---
+++ account_budget_workflow_policy/models/account_budget_workflow_policy.py
@@ -0,0 +1,48 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountBudgetWorkflowPolicy(models.Model):
+    _name = "crossovered.budget"
+    _inherit = [
+        "crossovered.budget",
+        "mixin.policy",
+    ]
+
+    # policy fields
+    confirm_ok = fields.Boolean(
+        string="Can Confirm",
+        compute="_compute_policy",
+    )
+
+    validate_ok = fields.Boolean(
+        string="Can Validate",
+        compute="_compute_policy",
+    )
+
+    approve_ok = fields.Boolean(
+        string="Can Approval",
+        compute="_compute_policy",
+    )
+
+    reject_ok = fields.Boolean(
+        string="Can Rejected",
+        compute="_compute_policy",
+    )
+
+    done_ok = fields.Boolean(
+        string="Can Done",
+        compute="_compute_policy",
+    )
+    cancel_ok = fields.Boolean(
+        string="Can Cancel",
+        compute="_compute_policy",
+    )
+
+    restart_ok = fields.Boolean(
+        string="Can Restart",
+        compute="_compute_policy",
+    )
