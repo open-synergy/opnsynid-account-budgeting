3de57f9531e4b16fa3dd226e196df964b4df190c
Andhitia Rama <andhitia.r@gmail.com>
2022-01-08 13:55:30 +0700

    [account_budget_multiple_approval] 11.0.1.1.0
    * Add standard approval template
    * Fix approval button position

--- account_budget_multiple_approval/__manifest__.py
+++ account_budget_multiple_approval/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Budget Multiple Approval",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
@@ -15,6 +15,7 @@
         "ssi_multiple_approval_mixin",
     ],
     "data": [
+        "data/approval_template_data.xml",
         "views/account_budget_views.xml",
     ],
 }
